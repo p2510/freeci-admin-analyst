8c3b59aa710b76a07243ef8c5df699b6426c663d
YATACHI <60470788+p2510@users.noreply.github.com>
2024-04-03 18:30:41 +0200

    change baseUrl

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,12 @@
-const baseUrl = 'http://127.0.0.1:8000'
+const baseUrl = 'https://stage.freeci.ci'
 const apiRoutes = {
   'analyst.variants': baseUrl + '/api/all-variants',
   'analyst.variants.today': baseUrl + '/api/all-variants-today',
   'analyst.variants.period': baseUrl + '/api/all-variants-period',
-  'gfreelancer': baseUrl + '/api/g-freelancer',
-  'gmission': baseUrl + '/api/g-mission',
-  'gcontract': baseUrl + '/api/g-contract',
-  'gturnover': baseUrl + '/api/g-turnover',
+  gfreelancer: baseUrl + '/api/g-freelancer',
+  gmission: baseUrl + '/api/g-mission',
+  gcontract: baseUrl + '/api/g-contract',
+  gturnover: baseUrl + '/api/g-turnover'
 }
 
 export { apiRoutes, baseUrl }
